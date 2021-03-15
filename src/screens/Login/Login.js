87ffc01258341e837247b8a2a26f81724410c653
Alok Ranjan <jalok25@gmail.com>
2021-03-15 21:24:43 +0530

    Header Completed, Integrated in the Login Page

---
+++ src/screens/Login/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Header from "../../common/header/Header"
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
